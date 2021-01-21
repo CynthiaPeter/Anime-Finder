9f44516c30af3e9e4270cdfe201ec0b0bd846bb5
Cynthia Peter <cynthiapeterpearl@gmail.com>
2021-01-21 15:23:36 +0100

    added the avatar glow and gesture

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -9,19 +10,42 @@ class HomePage extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Center(
-          child: Container(
-            
-            child: Material(
-              child: InkWell(
-                onTap: () {
-                  print("tapped");
-                },
-                child: Container(
-                  width: 200,
-                  height: 200,
+          child: AvatarGlow(
+            endRadius: 200,
+            glowColor: Colors.brown,
+            showTwoGlows: true,
+            animate: true,
+            child: Container(
+              child: GestureDetector(
+                onTap: (){},
+                              child: Material(
+                  elevation: 18,
+                  color: Colors.amber,
+                  shape: CircleBorder(),
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xff261416),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black87.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(1, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        'A',
+                        style: TextStyle(color: Colors.brown[200], fontSize: 70),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              // color: Colors.amber,
             ),
           ),
         ),
