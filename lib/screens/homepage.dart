cdb5c7c29663b55d2224652680b53272e581cd89
Cynthia Peter <cynthiapeterpearl@gmail.com>
2021-03-09 21:33:01 +0100

    A new project

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,55 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:avatar_glow/avatar_glow.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff261414),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Center(
-          child: AvatarGlow(
-            endRadius: 200,
-            glowColor: Colors.brown,
-            showTwoGlows: true,
-            animate: true,
-            child: Container(
-              child: GestureDetector(
-                onTap: (){},
-                              child: Material(
-                  elevation: 18,
-                  color: Colors.amber,
-                  shape: CircleBorder(),
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xff261416),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black87.withOpacity(0.5),
-                          spreadRadius: 1,
-                          blurRadius: 1,
-                          offset: Offset(1, 2), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        'A',
-                        style: TextStyle(color: Colors.brown[200], fontSize: 70),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
+      //backgroundColor: Color(0xff230916),
+     // backgroundColor: Color(0xff171717),
+      backgroundColor: Color(0xff181617),
+      
     );
   }
 }
